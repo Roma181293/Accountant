360270704a905cf583f3e409fee8b719faf9fde4
Roman Topchii <roma.topchiy@me.com>
2021-09-05 02:46:53 +0300

    removed unused files and code

--- Accountant/Cells/MoneyAccountTableViewCell.swift
+++ Accountant/Cells/MoneyAccountTableViewCell.swift
@@ -30,51 +30,9 @@ class MoneyAccountTableViewCell: UITableViewCell {
     }
     
     
-    func updateCell(dataToShow: (account: Account, title: String, amountInAccountCurrency: Double, amountInAccountingCurrency: Double), accountingCurrency : Currency) {
-        amountInBaseCurrency.text = ""
-        creditLimitAmount.text = ""
-        
-        guard let currency = dataToShow.account.currency else {return}
-        switch dataToShow.account.subType {
-        case AccountSubType.cash.rawValue :
-            accountIcon.image = UIImage(systemName: "bitcoinsign.circle")
-        case AccountSubType.debitCard.rawValue:
-            accountIcon.image = UIImage(systemName: "creditcard")
-        case AccountSubType.creditCard.rawValue:
-            accountIcon.image = UIImage(systemName: "creditcard.fill")
-            if let credit = dataToShow.account.linkedAccount {
-                let amount = round(AccountManager.balance(of: [credit])*100)/100
-                if amount != 0 {
-                    creditLimitAmount.text = "Credit limit: \(amount) \(credit.currency!.code!)"
-                }
-            }
-        case AccountSubType.deposit.rawValue:
-            accountIcon.image = UIImage(systemName: "hare")
-        default:
-            break
-        }
-        
-        nameLabel.text = dataToShow.title
-        
-        
-        if dataToShow.amountInAccountCurrency >= 0 {
-            amountLabel.text = "\(dataToShow.amountInAccountCurrency) \(currency.code!)"
-            amountLabel.textColor = .label
-            self.accessoryType = .none
-            if currency != accountingCurrency && dataToShow.amountInAccountingCurrency != 0 {
-                amountInBaseCurrency.text = "≈\(dataToShow.amountInAccountingCurrency) \(accountingCurrency.code!)"
-            }
-        }
-        else {
-            amountLabel.text = "\(dataToShow.amountInAccountCurrency) \(currency.code!)"
-            amountLabel.textColor = .systemRed
-            self.accessoryType = .detailButton
-        }
-    }
-    
-    
     func updateCell(dataToShow: AccountData, accountingCurrency : Currency) {
         accountIcon.tintColor = dataToShow.color
+        nameLabel.text = dataToShow.title
         amountInBaseCurrency.text = " "
         creditLimitAmount.text = " "
         
@@ -102,7 +60,7 @@ class MoneyAccountTableViewCell: UITableViewCell {
             break
         }
         
-        nameLabel.text = dataToShow.title
+        
         
         
         if dataToShow.amountInAccountCurrency >= 0 {
