d24c374d6ca4bfbadb3d07bae1f9a9c298e5e3ae
Roman Topchii <roma.topchiy@me.com>
2022-04-15 21:27:36 +0300

    code formatting in AppDelegate, CoreDataStack, ScreenDelegate files

--- Accountant/CoreDataStack.swift
+++ Accountant/CoreDataStack.swift
@@ -9,91 +9,88 @@ import Foundation
 import CoreData
 
 class CoreDataStack {
-    
+
     public static let modelName = "Accountant"
-    
+
     public static let model: NSManagedObjectModel = {
         let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd")!
         return NSManagedObjectModel(contentsOf: modelURL)!
     }()
-    
+
     static let shared = CoreDataStack()
-    private init(){}
-    
+
+    private init() {}
+
     public lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: CoreDataStack.modelName, managedObjectModel: CoreDataStack.model)
+
         let defaultDirectoryURL = NSPersistentContainer.defaultDirectoryURL()
-
-//        let testStoreURL = defaultDirectoryURL.appendingPathComponent("Test.sqlite")
-//        let testStoreDescription = NSPersistentStoreDescription(url: testStoreURL)
-//        testStoreDescription.configuration = "Test"
-
         let storeURL = defaultDirectoryURL.appendingPathComponent("Production.sqlite")
+
         let storeDescription = NSPersistentStoreDescription(url: storeURL)
         storeDescription.configuration = "Production"
         storeDescription.shouldMigrateStoreAutomatically = true
         storeDescription.shouldInferMappingModelAutomatically = false
 
+        let container = NSPersistentContainer(name: CoreDataStack.modelName, managedObjectModel: CoreDataStack.model)
         container.persistentStoreDescriptions = [storeDescription]
         container.loadPersistentStores(completionHandler: { (_, error) in
-//            guard let error = error as NSError? else { return }
-//            fatalError("###\(#function): Failed to load persistent stores:\(error)")
+            /*
+            guard let error = error as NSError? else { return }
+            fatalError("###\(#function): Failed to load persistent stores:\(error)")
+            */
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
-       
         return container
     }()
-    
-   
-    func switchToDB(_ db: Environment) {
-        
+
+    func switchToDB(_ environment: Environment) {
         persistentContainer = {
-            let container = NSPersistentContainer(name: CoreDataStack.modelName, managedObjectModel: CoreDataStack.model)
             let defaultDirectoryURL = NSPersistentContainer.defaultDirectoryURL()
+            let storeURL = defaultDirectoryURL.appendingPathComponent("\(environment.rawValue).sqlite")
 
-            let storeURL = defaultDirectoryURL.appendingPathComponent("\(db.rawValue).sqlite")
             let storeDescription = NSPersistentStoreDescription(url: storeURL)
-            storeDescription.configuration = db.rawValue
+            storeDescription.configuration = environment.rawValue
+            storeDescription.shouldMigrateStoreAutomatically = true
+            storeDescription.shouldInferMappingModelAutomatically = false
 
+            let container = NSPersistentContainer(name: CoreDataStack.modelName,
+                                                  managedObjectModel: CoreDataStack.model)
             container.persistentStoreDescriptions = [storeDescription]
             container.loadPersistentStores(completionHandler: { (_, error) in
-//                guard let error = error as NSError? else { return }
-//                fatalError("###\(#function): Failed to load persistent stores:\(error)")
+                /*
+                guard let error = error as NSError? else { return }
+                fatalError("###\(#function): Failed to load persistent stores:\(error)")
+                */
             })
             container.viewContext.automaticallyMergesChangesFromParent = true
-           
             return container
         }()
-        
         UserProfile.setDateOfLastChangesInDB(Date())
     }
-    
-    
+
     func activeEnviroment() -> Environment? {
         if persistentContainer.persistentStoreDescriptions.count == 0 {
             return nil
         }
-        
         if persistentContainer.persistentStoreDescriptions[0].configuration == Environment.test.rawValue {
             return Environment.test
-        }
-        else {
+        } else {
             return Environment.prod
         }
     }
-    
-    
+
     public func saveContext(_ context: NSManagedObjectContext) throws {
         if context.hasChanges {
             do {
                 try context.save()
                 UserProfile.setDateOfLastChangesInDB(Date())
-            } catch {
+            } catch let error {
                 context.rollback()
-//                 Replace this implementation with code to handle the error appropriately.
-//                 fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                let nserror = error as NSError
-//                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                throw error
+                /*
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                */
             }
         }
     }
