aa25683bc2e9d6789542a8f847b3396b7ed40c5b
Roman Topchii <roma.topchiy@gmail.com>
2023-11-26 11:38:32 +0200

    applying MVVM to Welcome scene

---
+++ Accountant/Utilities/Dynamic.swift
@@ -0,0 +1,28 @@
+//
+//  Dynamic.swift
+//  Accountant
+//
+//  Created by Roman Topchii on 26.11.2023.
+//
+
+import Foundation
+
+class Dynamic<T> {
+    typealias Listener = (T) -> Void
+    
+    private var listener: Listener?
+    
+    func bind(_ listener: Listener?) {
+        self.listener = listener
+    }
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+}
